1050dd93c73f220997c09d9dea5437890fb647ae
zhukata <zhukata@gmail.com>
2024-07-07 17:03:29 +0300

    two commit

--- hexlet_flask/app.py
+++ hexlet_flask/app.py
@@ -1,13 +1,17 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, flash, render_template, request, url_for, redirect
 
-# Это callable WSGI-приложение
+from hexlet_flask import users
+
 app = Flask(__name__)
 
-users = ['mike', 'mishel', 'adel', 'keks', 'kamila']
+app.config['SECRET_KEY'] = 'secret key'
+
+users_list = ['mike', 'mishel', 'adel', 'keks', 'kamila']
+
 
 @app.route('/')
 def index():
-    return 'Welcome to Flask!'
+    return render_template('form.html')
 
 
 @app.get('/users')
@@ -18,11 +22,26 @@ def users_get():
         'users/index.html',
         users=users,
         search=term,
-        filtred_users=filter_users(users, term)
+        filtred_users=users.filter_users(users_list, term)
     )
 
 
-@app.route('/users/<id>')
+@app.post('/users')
+def post_users():
+    user = request.form.to_dict()
+    errors = users.validate(user)
+    if errors:
+        return render_template(
+          'users/new.html',
+          user=user,
+          errors=errors,
+        ), 422
+    
+    # repo.save(user)
+    flash('Пользователь успешно добавлен', 'success')
+    return redirect(url_for('users_get'), code=302)
+
+@app.get('/users/int:<id>')
 def user_id(id):
 
     return render_template(
@@ -31,18 +50,16 @@ def user_id(id):
     )
 
 
-@app.route('/courses/<id>')
-def courses(id):
-    return f'Course id: {id}'
+@app.get('/users/new')
+def get_user():
+    user = {
+        'name': '',
+        'email' : ''
+    }
+    errors = {}
 
-
-def filter_users(users, term):
-    filtred_users = []
-    if not term:
-        return users
-    for user in users:
-        # if term.lower() in i['first_name'].lower():
-        #     filtred_users.append(i['first_name'])
-        if user.lower().startswith(term.lower()):
-            filtred_users.append(user)
-    return filtred_users
+    return render_template(
+        'users/new.html',
+        user=user,
+        errors=errors
+    )
